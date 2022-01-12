f8b8c6125c896d538a74f2499ce9bcdcfe580c02
Astone Tuhame <astonetuhame@gmail.com>
2022-01-12 22:29:34 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <Tutorial />
+  <div>
+    <h2>Welcome to the best dad jokes on the web</h2>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  head() {
+    return {
+      title: 'Welcome to dad jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for dad jokes',
+        },
+      ],
+    }
+  },
 }
 </script>
