cb0337824e9bfa43e9c8742f8c6c6a75276a7815
Astone Tuhame <astonetuhame@gmail.com>
2022-01-12 22:29:27 +0300

    Create about.vue

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>This is an app that displays dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for dad jokes',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
